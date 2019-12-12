dc57cece97e2737e05aeeb7ed0a815f3cc6c5421
GowthamanJegan <gowthambe91@gmail.com>
2019-12-12 18:06:19 +0530

    Startup and Web Host builder - https://www.tutorialsteacher.com/core/aspnet-core-startup

--- MyFirstNetCoreApp/MyFirstNetCoreApp/Program.cs
+++ MyFirstNetCoreApp/MyFirstNetCoreApp/Program.cs
@@ -14,24 +14,72 @@ namespace MyFirstNetCoreApp
     {
         public static void Main(string[] args)
         {
-            /*BuildWebHost(args).Run();*/ // default when created automatically
+            BuildWebHost(args).Run(); // default when created automatically ; This is in Asp.Net Core 2.x, Creates Web host to run the Asp Net core web app
             /*Basically ASP.Net Core Web Application is a console application, when i close the browser tab, program stops automatically*/
             // Have renamed the wwwroot folder to root, no issues runs successfully
-            var host = new WebHostBuilder() //Web Host Builder is the helper class helps to create and configure web host for the web application
-                .UseKestrel() /*specify use kestrel as the web server to be used by the web host! Kestrel is an open source, cross-platform web server for Asp.Net Core, 
-                Asp.Net Core can be a cross platform web application, so it can be used with any web server not only with IIS. Hence there will be an external web server IIS, Apache, Nginx
-                etc., which will dispatch http requests to the internal web server Kestrel.*/
-                .UseContentRoot(Directory.GetCurrentDirectory()) // specify the content root directory to be used by the web host, which contains the static/content files such as CSS,images,js files.
-                .UseIISIntegration() //configures the port and the base path the server should listen on when running behind AspNetCoreModule 
-                .UseStartup<Startup>() //specifies the start up type to be used by the web host
-                .Build();//Builds an Iwebhost which hosts a web application
 
-            host.Run();
+            /*Below is the code for creating web host in Asp.Net Core 1.x*/
+            //var host = new WebHostBuilder() //Web Host Builder is the helper class helps to create and configure web host for the web application
+            //    .UseKestrel() /*specify use kestrel as the web server to be used by the web host! Kestrel is an open source, cross-platform web server for Asp.Net Core, 
+            //    Asp.Net Core can be a cross platform web application, so it can be used with any web server not only with IIS. Hence there will be an external web server IIS, Apache, Nginx
+            //    etc., which will dispatch http requests to the internal web server Kestrel.*/
+            //    .UseContentRoot(Directory.GetCurrentDirectory()) // specify the content root directory to be used by the web host, which contains the static/content files such as CSS,images,js files.
+            //    .UseIISIntegration() //configures the port and the base path the server should listen on when running behind AspNetCoreModule 
+            //    .UseStartup<Startup>() //specifies the start up type to be used by the web host
+            //    .Build();//Builds an Iwebhost which hosts a web application
+
+            //host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            WebHost.CreateDefaultBuilder(args) /* Web host is a static class which can be used for creating an instance of IWebhost and IWebhostbuilder with pre configured defaults.   
+                                               CreateDefaultBuilder() creates the new instance of web host builder with preconfigured defaults, internally it configures Kestrel, IISIntegration and 
+                                               other configurations*/
                 .UseStartup<Startup>()
                 .Build();
     }
+
+    /*Following is the Source code for CreateDefaultBuilder method from github*/
+    //public static IWebHostBuilder CreateDefaultBuilder(string[] args)
+    //{
+    //    var builder = new WebHostBuilder()
+    //        .UseKestrel()
+    //        .UseContentRoot(Directory.GetCurrentDirectory())
+    //        .ConfigureAppConfiguration((hostingContext, config) =>
+    //        {
+    //            var env = hostingContext.HostingEnvironment;
+
+    //            config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    //                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+    //            if (env.IsDevelopment())
+    //            {
+    //                var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+    //                if (appAssembly != null)
+    //                {
+    //                    config.AddUserSecrets(appAssembly, optional: true);
+    //                }
+    //            }
+
+    //            config.AddEnvironmentVariables();
+
+    //            if (args != null)
+    //            {
+    //                config.AddCommandLine(args);
+    //            }
+    //        })
+    //        .ConfigureLogging((hostingContext, logging) =>
+    //        {
+    //            logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    //            logging.AddConsole();
+    //            logging.AddDebug();
+    //        })
+    //        .UseIISIntegration()
+    //        .UseDefaultServiceProvider((context, options) =>
+    //        {
+    //            options.ValidateScopes = context.HostingEnvironment.IsDevelopment();
+    //        });
+
+    //    return builder;
+    //}
 }
--- MyFirstNetCoreApp/MyFirstNetCoreApp/Startup.cs
+++ MyFirstNetCoreApp/MyFirstNetCoreApp/Startup.cs
@@ -15,11 +15,18 @@ namespace MyFirstNetCoreApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            /*This is where we register the Dependent classes with IoC container. Dependancy injection is heavily used by the Asp.Net Core Architecture. After registering we can use them anywhere 
+             * in the application, we just have to include it in the parameter of the constructor of the class, IoC container will automatically inject the dependant class.
+             Asp.Net Core refers dependant classes as service, so whenever we read service , consider it as a class going to be used in some other class.
+             ConfigureServices method includes IServiceCollection parameter to register services to the IoC container.*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) //These parameters are framework services(classes) injected by built in IoC container.
         {
+            /*Configure is the place where we can configure application request pipeline using IApplicationBuilder instance that is provided by the built in IoC container. Asp.Net Core introduced the
+             * Middleware concept which helps to define the request pipeline  which will be executed on every request. So we include only those necessary middleware components , thus increase the 
+             performance of the application. */
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -30,5 +37,7 @@ namespace MyFirstNetCoreApp
                 await context.Response.WriteAsync("Hello World!");
             });
         }
+
+        /*At runtime Configure Services method will be called before Configure method -  so that all the dependancies are registered before that may be used in the configure method. */
     }
 }
